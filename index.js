2ef329df4a172e3b29b348d9747a97ae50fe6b30
T0ZSKY <tomlimon1508@gmail.com>
2024-04-03 21:38:52 +0200

    update

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-document.getElementById("login-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var alertBox = document.getElementById("alert");
-    var alertMessage = document.getElementById("alert-message");
+// Ajout d'un écouteur d'événement pour le formulaire de connexion
+document.getElementById("login-form").addEventListener("submit", function(a) {
+    a.preventDefault(); // Empêche le formulaire de se soumettre normalement
 
-    // Vérifiez si les identifiants sont corrects
-    if (username === "Tom" && password === "LeMdpDeTom") {
-        // Stockez l'état de connexion dans localStorage
+    // Récupération des valeurs du nom d'utilisateur et du mot de passe
+    var b = document.getElementById("username").value;
+    var c = document.getElementById("password").value;
+
+    // Sélection des éléments d'alerte pour afficher les messages d'erreur
+    var d = document.getElementById("alert");
+    var e = document.getElementById("alert-message");
+
+    // Vérification des identifiants
+    if (b === hexToString("546f6d") && c === hexToString("4c654d64704465546f6d")) {
+        // Si les identifiants sont corrects, stockez l'état de connexion dans localStorage
         localStorage.setItem("isLoggedIn", true);
         // Redirigez l'utilisateur vers la page principale
         window.location.href = "./src/main/main.html";
     } else {
-        // Affichez un message d'erreur
-        alertMessage.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
-        alertBox.classList.add("show");
+        // Si les identifiants sont incorrects, affichez un message d'erreur
+        e.textContent = hexToString("4e6f6d206475277573657275696f2071756520706173736520696e636f72726563742e");
+        d.classList.add("show");
         setTimeout(function() {
-            alertBox.classList.remove("show");
+            d.classList.remove("show");
         }, 3000);
-        // Ajoutez une animation de secousse
+        // Ajoutez une animation de secousse au conteneur de connexion
         document.querySelector(".login-container").style.animation = "shake 0.5s";
         setTimeout(function() {
             document.querySelector(".login-container").style.animation = "";
         }, 500);
     }
 });
+
+// Fonction pour convertir une chaîne hexadécimale en texte brut
+function hexToString(hex) {
+    var string = '';
+    for (var i = 0; i < hex.length; i += 2) {
+        string += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
+    return string;
+}
