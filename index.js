43a9074f6454479bf474408f2769cc461f3c5a5e
T0ZSKY <tomlimon1508@gmail.com>
2024-04-03 20:09:16 +0200

    add cookie security

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-document.getElementById("login-form").addEventListener("submit", function(a) {
-    a.preventDefault();
-    var b = document.getElementById("username").value,
-        c = document.getElementById("password").value,
-        d = document.getElementById("alert"),
-        e = document.getElementById("alert-message"),
-        f = "546f6d", // "Tom" en hexadécimal
-        g = "4c654d64704465546f6d"; // "Admin" en hexadécimal
-    if (b === hexToString(f) && c === hexToString(g)) {
-        window.location.href = "/src/main/main.html";
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var alertBox = document.getElementById("alert");
+    var alertMessage = document.getElementById("alert-message");
+
+    // Vérifiez si les identifiants sont corrects
+    if (username === "Tom" && password === "LeMdpDeTom") {
+        // Stockez l'état de connexion dans localStorage
+        localStorage.setItem("isLoggedIn", true);
+        // Redirigez l'utilisateur vers la page principale
+        window.location.href = "./src/main/main.html";
     } else {
-        e.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
-        d.classList.add("show");
+        // Affichez un message d'erreur
+        alertMessage.textContent = "Nom d'utilisateur ou mot de passe incorrect.";
+        alertBox.classList.add("show");
         setTimeout(function() {
-            d.classList.remove("show");
-        }, 3e3);
+            alertBox.classList.remove("show");
+        }, 3000);
+        // Ajoutez une animation de secousse
         document.querySelector(".login-container").style.animation = "shake 0.5s";
         setTimeout(function() {
             document.querySelector(".login-container").style.animation = "";
         }, 500);
     }
 });
-
-function hexToString(hex) {
-    var string = '';
-    for (var i = 0; i < hex.length; i += 2) {
-        string += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    }
-    return string;
-}
--- src/main/main_scrypt.js
+++ src/main/main_scrypt.js
@@ -1,9 +1,8 @@
-
-
+// Fonction pour envoyer un e-mail de demande de contact
 function envoyerEmail() {
     var destinataire = 'tomlimon1508@gmail.com';
     var sujet = 'Demande de contact';
-    var corps = 'Je voudrais une demo de votre application. Merci de me contacter au plus vite. Cordialement.';
+    var corps = 'Je voudrais une démo de votre application. Merci de me contacter au plus vite. Cordialement.';
 
     var mailtoURL = 'mailto:' + destinataire + '?subject=' + encodeURIComponent(sujet) + '&body=' + encodeURIComponent(corps);
     
@@ -11,7 +10,7 @@ function envoyerEmail() {
     window.location.href = mailtoURL;
 }
 
-
+// Fonction pour envoyer un e-mail de demande de support
 function support() {
     var destinataire = 'tomlimon1508@gmail.com';
     var sujet = 'Demande de Support';
@@ -21,4 +20,18 @@ function support() {
     
     // Ouvrir l'application de messagerie par défaut avec les paramètres
     window.location.href = mailtoURL;
+}
+
+// Vérification de l'état de connexion lorsque la page se charge
+document.addEventListener("DOMContentLoaded", function() {
+    if (!isLoggedIn()) {
+        // Si l'utilisateur n'est pas authentifié, redirigez-le vers la page de connexion
+        window.location.href = "../../index.html";
+    }
+});
+
+// Fonction pour vérifier l'état de connexion
+function isLoggedIn() {
+    // Vérifiez si l'état de connexion est présent dans localStorage
+    return localStorage.getItem("isLoggedIn") === "true";
 }
